b0418fcb8cfa4b3734d98da48edc386212824749
Francois <durss@free.fr>
2020-01-21 21:51:02 +0100

    Managing OAuth access denied case

--- front_src/views/OAuth.vue
+++ front_src/views/OAuth.vue
@@ -1,39 +1,48 @@
 <template>
 	<div class="oauth">
-		
+		<div v-if="isError" class="error">
+			<p class="title">You must grant access to your playlists to play Multiblind Test</p>
+			<p class="infos">Don't worry, <strong>Multiblind Test</strong> only requests for playlists <strong>read</strong> permission, it will never be able to change anything on your spotify account.</p>
+		</div>
+		<Home v-if="isError" />
 	</div>
 </template>
 
 <script lang="ts">
 import { Component, Inject, Model, Prop, Vue, Watch, Provide } from "vue-property-decorator";
-import PlaylistSelector from './PlaylistSelector.vue';
 import SpotifyAPI from '../utils/SpotifyAPI';
+import Home from './Home.vue';
+import Utils from '../utils/Utils';
 
 @Component({
 	components:{
-		PlaylistSelector,
+		Home,
 	}
 })
 export default class OAuth extends Vue {
 
-	public mounted():void {
-		let redirect = localStorage.getItem("redirect");
-		console.log("REDIR :: "+redirect)
-		//Convert hash to key/value object
-		let vars = JSON.parse('{"' + decodeURI(document.location.hash.substring(1)).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g,'":"') + '"}')
-		if(vars.access_token) {
-			
-			this.$store.dispatch("authenticate", {access_token:vars.access_token});
+	public isError:boolean = false;
 
-			if(redirect) {
-				window.location.href = redirect;
-				localStorage.removeItem("redirect");
-			}else{
-				//Redirect to playlist selector
-				this.$router.push({name:"playlists"});
+	public mounted():void {
+		let error = Utils.getQueryParameterByName("error");
+		if(!error && document.location.hash) {
+
+			//Convert hash to key/value object
+			let vars = JSON.parse('{"' + decodeURI(document.location.hash.substring(1)).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g,'":"') + '"}')
+			if(vars.access_token) {
+				let redirect = localStorage.getItem("redirect");
+				this.$store.dispatch("authenticate", {access_token:vars.access_token});
+	
+				if(redirect) {
+					window.location.href = redirect;
+					localStorage.removeItem("redirect");
+				}else{
+					//Redirect to playlist selector
+					this.$router.push({name:"playlists"});
+				}
 			}
 		}else{
-			//TODO
+			this.isError = true;
 		}
 	}
 
@@ -48,5 +57,16 @@ export default class OAuth extends Vue {
 <style scoped lang="less">
 @import (reference) '../less/_includes.less';
 .oauth{
+	.error {
+		color: #fff;
+		border-radius: 10px;
+		padding: 10px;
+		background-color: @mainColor_warn;
+		.title {
+			font-family: "FuturaExtraBold";
+			text-align: center;
+			margin-bottom: 10px;
+		}
+	}
 }
 </style>
